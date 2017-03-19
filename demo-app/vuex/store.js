174555b2c7debd50cc95ce3c6bcf5006d6cb6a76
Greg Hatt <groggle@yandex.com>
2017-03-19 00:26:29 -0300

    Add comment to Vuex store

---
+++ demo-app/vuex/store.js
@@ -0,0 +1,18 @@
+let Vue = require('vue')
+let Vuex = require('vuex')
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+// Strict prevents modifying the store outside of
+// the mutation methods. The store is available in
+// components by using either maps(mapgetters/mapstate)
+// or by directily referencing the store as this.$store
+store  = new Vuex.Store({
+  modules: {
+  },
+  strict: debug
+})
+
+module.exports = store
